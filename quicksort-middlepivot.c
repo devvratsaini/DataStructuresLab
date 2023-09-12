fc9516a4db53a373bdef313ab0af5e8c3f6b1ce3
Devvrat Saini <111595511+devvratsaini@users.noreply.github.com>
2023-09-12 19:41:11 +0530

    Create quicksort-middlepivot.c

---
+++ quicksort-middlepivot.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+void swap(int *x, int *y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void printArray(int arr[], int size) {
+	for(int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+}
+
+int quicksort_middle(int arr[], int low, int high) {
+	if(low < high) {
+		int pivot = (low+high)/2, i = low, j = high;
+		while (i < j) {
+			while (arr[i] <= arr[pivot] && i <= high)
+				i++;
+			while (arr[j] > arr[pivot] && j >= low)
+				j--;
+			if(i < j)
+				swap(&arr[i], &arr[j]);
+		}
+		swap(&arr[j], &arr[pivot]);
+		quicksort_middle(arr, low, pivot - 1);
+		quicksort_middle(arr, pivot + 1, high);
+	}
+}
+
+int main() {
+	int size;
+	printf("Enter the number of elements in the array: ");
+	scanf("%d", &size);
+	
+	int arr[size];
+	printf("Enter elements of the array:\n");
+	for(int i = 0; i < size; i++)
+		scanf("%d", &arr[i]);
+	
+	quicksort_middle(arr, 0, size - 1);
+	
+	printf("Sorted array is: ");
+	printArray(arr, size);
+	
+	return 0;
+}

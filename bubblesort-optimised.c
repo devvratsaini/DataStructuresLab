96a228ff04b901b4695c5a5be9eefa8923f2d788
Devvrat Saini <111595511+devvratsaini@users.noreply.github.com>
2023-09-24 14:47:40 +0530

    Update bubblesort-optimised.c

--- bubblesort-optimised.c
+++ bubblesort-optimised.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 
-//swaps value of two integers
+// swap two integers function
 void swap(int *x, int *y) {
 	int temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
-//optimized bubble sort of array
+// optimized bubble sort function
 void bubbleSort(int arr[], int size) {
 	int i, j;
 	for(i=0; i<size-1; i++) {
 		int swapped = 0;
 		for(j=0; j<size-i-1; j++) {
-			//swap if smaller value is found
+			// swap if smaller value is found
 			if(arr[j] > arr[j+1]) {
 				swap(&arr[j], &arr[j+1]);
 				swapped = 1;
 			}
 		}
 
-		
+	// break if array is sorted
 	if(swapped == 0)
 		break;
 	}
@@ -30,21 +30,21 @@ void bubbleSort(int arr[], int size) {
 int main() {
 	printf("OPTIMIZED BUBBLE SORT\n\n");
 	
-	//input: size of array
+	// input: size of array
 	int size;
 	printf("Enter the number of elements in array: ");
 	scanf("%d", &size);
 	
-	//input: elements of array
+	// input: elements of array
 	int i, arr[size];
 	printf("Enter the elements of the array: ");
 	for(i = 0; i < size; i++)
 		scanf("%d",&arr[i]);
 	
-	//sorting the array
+	// sorting the array
 	bubbleSort(arr, size);
 	
-	//print: array
+	// print: array
 	printf("Sorted array: ");
 	for(i=0; i<size; i++)
 		printf("%d ", arr[i]);

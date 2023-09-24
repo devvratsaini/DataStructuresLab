bdebcf03d5e3c8f3bd6db2a9ebc6b88cd56fff46
Devvrat Saini <111595511+devvratsaini@users.noreply.github.com>
2023-09-24 14:51:13 +0530

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
+// function to print an array in order
 void printArray(int arr[], int size) {
 	int i;
 	for(i=0; i<size; i++)
 		printf("%d ", arr[i]);
 }
 
+// insertion sort function
 void insertionSort(int arr[], int size) {
 	int i, j, key;
-	
 	for (i=1; i<size; i++) {
 		key = arr[i];
 		j = i-1;
@@ -21,19 +22,24 @@ void insertionSort(int arr[], int size) {
 	}
 }
 
+// main function
 int main() {
+	//input: size of array
 	int size;
 	printf("Enter the number of elements in the array: ");
 	scanf("%d", &size);
-	
+
+	// input: array elements
 	int arr[size];
 	printf("Enter elements of the array:\n");
 	int i;
 	for(i = 0; i < size; i++)
 		scanf("%d", &arr[i]);
-	
+
+	// sorting the array
 	insertionSort(arr, size);
 	
+	// printint the sorted array
 	printf("Sorted array is: ");
 	printArray(arr, size);
 	

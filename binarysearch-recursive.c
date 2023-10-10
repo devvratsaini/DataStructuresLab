223c1708857ff43fb4fb7f64be99b68ebab32ef2
Devvrat Saini <111595511+devvratsaini@users.noreply.github.com>
2023-10-10 23:36:25 +0530

    Update binarysearch-recursive.c

--- binarysearch-recursive.c
+++ binarysearch-recursive.c
@@ -11,7 +11,7 @@ FOR BINARY SEARCH:
 #include<stdio.h>
 
 //function implementing recursive binary search
-int binSearchRec(int key, int arr[], int low, int high) {
+int binary_search_recursive(int key, int arr[], int low, int high) {
 	if(low<=high) {
 		int mid = (high + low)/2;
 		
@@ -21,10 +21,10 @@ int binSearchRec(int key, int arr[], int low, int high) {
 		
 		//if key is lesser than mid, ignore right half
 		if(arr[mid]>key)
-			return binSearchRec(key, arr, low, mid-1);
+			return binary_search_recursive(key, arr, low, mid-1);
 			
 		//if key is greater than mid, ignore left half
-		return binSearchRec(key, arr, mid+1, high);
+		return binary_search_recursive(key, arr, mid+1, high);
 	}
 	//else return -1 if not found
 	return -1;
@@ -48,7 +48,7 @@ int main() {
 	scanf("%d", &key);
 	
 	//calling recursive binary search function
-	int result = binSearchRec(key, arr, 0, size-1);
+	int result = binary_search_recursive(key, arr, 0, size-1);
 	if(result==-1)
 		printf("%d is not present in the array.", key);
 	else

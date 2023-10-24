b073c3d063987873162a3ccca409e9c4d5f1f401
Devvrat Saini <111595511+devvratsaini@users.noreply.github.com>
2023-10-24 14:32:29 +0530

    Update selectionsort.c

--- Searching and Sorting/selectionsort.c
+++ Searching and Sorting/selectionsort.c
@@ -1,46 +1,66 @@
+/*
+CODE FOR IMPLEMENTAION OF SELECTION SORT (ITERATIVE)
+	~WRITTEN BY DEVVRAT SAINI
+
+TIME COMPLEXITY FOR SELECTION SORT:
+	BEST CASE = O (N^2)
+	AVERAGE CASE = O (N^2)
+	WORST CASE = O (N^2)
+*/	
+
 #include<stdio.h>
 
-void swap(int *x, int *y) {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-void printArray(int arr[], int size) {
-	int i;
-	for(i = 0; i < size; i++)
-		printf("%d ", arr[i]);
-}
-
-void selectionSort(int arr[], int size) {
+// Function to implement Selection Sort
+void selectionSort(int arr[] ,int n) {
 	int i, j, imin;
 	
-	for(i=0; i<size-1; i++) {
+	for (i = 0; i < n-1; i++) {
 		imin = i;
 		
-		for(j=i+1; j<size; j++)
-			if(arr[j] < arr[imin])
-				imin = j;
+		for (j = i+1; j < n; j++) {
+			if (arr[j] < arr[imin]) {
+				imin=j;
+			}
+		}
 		
-		swap(&arr[i], &arr[imin]);
+		// swapping
+		int temp;
+		temp = arr[i];
+		arr[i] = arr[imin];
+		arr[imin] = temp;
 	}
 }
 
-int main() {
-	int size;
-	printf("Enter the number of elements in the array: ");
-	scanf("%d", &size);
-	
-	int arr[size];
-	printf("Enter elements of the array:\n");
+// Function to print a given array
+void printArray(int arr[], int n) {
 	int i;
-	for(i = 0; i < size; i++)
-		scanf("%d", &arr[i]);
 	
-	selectionSort(arr, size);
+	for(i = 0; i < n; i++) {
+	    printf("%d ",arr[i]);
+	}
 	
-	printf("Sorted array is: ");
-	printArray(arr, size);
+	printf("\n");
+}
 
-	return 0;
+// Main function
+int main() {
+	int i, arr[50], n;
+	
+	printf("Enter number of elements: ");
+	scanf("%d", &n);
+	
+	// Taking elements of array as input
+	printf("Enter elements of the array: ");
+	for(i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	
+	// Sorting the array
+	selectionSort(arr, n);
+	
+	// Printing sorted array
+	printf("\nSorted Array: ");
+	printArray(arr, n);
+	
+	return 0;	
 }
